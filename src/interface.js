7a556b5a0066b72685f2364e89d0cb833df8d65e
Joe Marriott <marriott90@gmail.com>
2017-02-02 20:10:15 +0000

    weather API integrated

--- src/interface.js
+++ src/interface.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
     updateTemperature();
     updatePowerSaveStatus();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=Doncaster&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+      $('#current-temperature').text(data.main.temp + ' degrees celsius');
+    });
+    $('#current-city').change(function() {
+      var city = $('#current-city').val();
+      $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+        $('#current-temperature').text(data.main.temp + ' degrees celsius')
+      });
+    });
 
     $('#temperature-up').click(function() {
         thermostat.increase();
